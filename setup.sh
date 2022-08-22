da5f5adf17b6e1fd7a1a25a34764eb4e621daad0
Vitor Anjos <bartier@users.noreply.github.com>
2022-08-22 18:19:03 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# Home-brew
+# Install Home-brew
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
@@ -10,31 +10,34 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git config --global user.name "Vitor Anjos"
 git config --global user.email "bartier@users.noreply.github.com"
 
-brew install jq
-brew install awscli
-brew install htop
-brew install tree
-brew install wget
-brew install kubectx
-brew install kube-ps1
-brew install warrensbox/tap/tfswitch
-brew install tfsec
-brew install pre-commit
-brew install helm
-brew install ipcalc
-brew install derailed/k9s/k9s
+brew bundle -f
+brew bundle --force cleanup
 
-brew install --cask authy
-brew install --cask spotify
-brew install --cask discord
-brew install --cask mongodb-compass
-brew install --cask slack
-brew install --cask docker
-brew install --cask google-cloud-sdk
-brew install --cask rectangle
-brew install --cask iterm2
-brew install --cask jetbrains-toolbox
-brew install --cask google-chrome
+# brew install jq
+# brew install awscli
+# brew install htop
+# brew install tree
+# brew install wget
+# brew install kubectx
+# brew install kube-ps1
+# brew install warrensbox/tap/tfswitch
+# brew install tfsec
+# brew install pre-commit
+# brew install helm
+# brew install ipcalc
+# brew install derailed/k9s/k9s
+
+# brew install --cask authy
+# brew install --cask spotify
+# brew install --cask discord
+# brew install --cask mongodb-compass
+# brew install --cask slack
+# brew install --cask docker
+# brew install --cask google-cloud-sdk
+# brew install --cask rectangle
+# brew install --cask iterm2
+# brew install --cask jetbrains-toolbox
+# brew install --cask google-chrome
 
 # Dock settings
 #TODO
