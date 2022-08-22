a0c5d7f1f369e9bd03ea63c24dec6b742402b4a6
Vitor Anjos <vitoranjos@Vitors-MacBook-Pro.local>
2022-08-22 19:06:51 -0300

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 # Install Home-brew
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Install Oh My ZSH
